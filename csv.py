c4b4bcfc0cf9d552f133a5490c5edc7047de0d29
Magnus Valle <wiscados@gmail.com>
2012-02-25 11:30:44 +0000

    names starting with a number are prefixed with '_'

--- csv.py
+++ csv.py
@@ -1,3 +1,5 @@
+import re
+
 class Csv:
     values = []
     def __init__(self, file_name):
@@ -15,10 +17,14 @@ class Csv:
             v.__autoadd__(line)
             yield v
 
-    def _checkHeaderName(self, name, allocated_names):
+    def _sanitize(self, name, allocated_names):
         name = name.strip().replace(" ", "_")
         if allocated_names.has_key(name):
             raise NameError("%s is already defined" % name)
+        if re.match(r'^[0-9]', name):
+            name = "_"+name
+        elif name == "":
+            print "Warning! Empty header in file"
         return name
 
     def _generateValueClass(self, headers):
@@ -26,14 +32,12 @@ class Csv:
             def __autoadd__(self, data_line):    
                 for (i, datum) in enumerate(data_line.split(',')):
                     setattr(self, self.__attributemap__[i], datum)
-
-
         attributes = {}
         attribute_map = {}
         attributes["__attributemap__"] = attribute_map
         
         for (i, header) in enumerate(headers):
-            header = self._checkHeaderName(header, attribute_map)
+            header = self._sanitize(header, attribute_map)
             attributes[header] = ""
             attribute_map[i] = header
 
--- test.py
+++ test.py
@@ -3,3 +3,5 @@ from csv import Csv
 csv = Csv("test.csv")
 for i in csv.read():
     print i.Name
+    print dir(i)
+    print i._2nd_name
